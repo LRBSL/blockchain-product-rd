52bfe35e598dec1c9fa368fd8675d3c02eb1a940
RavinduSachintha <ravindusachintha53@gmail.com>
2019-10-09 09:48:11 +0000

    server initialized

---
+++ land_registration/packages/server/src/controllers/router.ts
@@ -0,0 +1,15 @@
+import * as express from 'express';
+import { 
+    LandController_init_post,
+    LandController_queryLand_get,
+    LandController_queryAllLands_get,
+    LandController_createLand_post,
+    LandController_changeLandOwner_post,
+    LandController_getHistoryForLand_get } from './controllers'
+export default express.Router()
+.post('/land/init', LandController_init_post)
+.get('/land/queryLand/:id', LandController_queryLand_get)
+.get('/land/queryAllLands', LandController_queryAllLands_get)
+.post('/land/createLand', LandController_createLand_post)
+.post('/land/changeLandOwner', LandController_changeLandOwner_post)
+.get('/land/getHistoryForLand/:id', LandController_getHistoryForLand_get)
