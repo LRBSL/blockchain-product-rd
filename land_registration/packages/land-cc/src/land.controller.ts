3e66a9a4330f5518dc9a75d051b45a1f7e338c49
RavinduSachintha <ravindusachintha53@gmail.com>
2019-10-09 13:30:55 +0530

    controller init method

--- land_registration/packages/land-cc/src/land.controller.ts
+++ land_registration/packages/land-cc/src/land.controller.ts
@@ -1,3 +1,4 @@
+import * as yup from 'yup';
 import { ChaincodeTx } from '@worldsibu/convector-platform-fabric';
 import {
   Controller,
@@ -10,6 +11,22 @@ import { Land } from './land.model';
 
 @Controller('land')
 export class LandController extends ConvectorController<ChaincodeTx> {
+
+  @Invokable()
+  public async init() {
+    // Mock Data
+    let mockData = [
+      new Land({ RLRegistry: "Colombo", Extent: 50, ParentLandID: "nil", Owner: "Tomoko", Boundaries: [[0,20],[10,20],[10,0],[0,0]]}),
+      new Land({ RLRegistry: "Delkanda", Extent: 25, ParentLandID: "nil", Owner: "Brad", Boundaries: [[0,20],[10,20],[10,0],[0,0]]})
+    ];
+    await Promise.all(mockData.map(land => land.save()));
+  }
+
+  @Invokable()
+  public async query(@Param(yup.string()) id: string): Promise<Land> {
+    return Land.getOne(id);
+  }
+
   @Invokable()
   public async create(
     @Param(Land)
