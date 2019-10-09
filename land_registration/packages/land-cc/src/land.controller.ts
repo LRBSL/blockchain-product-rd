5d4b56ab11c24b8c523d421f5076148e0bfbab90
RavinduSachintha <ravindusachintha53@gmail.com>
2019-10-09 13:38:25 +0530

    update id in init function

--- land_registration/packages/land-cc/src/land.controller.ts
+++ land_registration/packages/land-cc/src/land.controller.ts
@@ -16,8 +16,8 @@ export class LandController extends ConvectorController<ChaincodeTx> {
   public async init() {
     // Mock Data
     let mockData = [
-      new Land({ RLRegistry: "Colombo", Extent: 50, ParentLandID: "nil", Owner: "Tomoko", Boundaries: [[0,20],[10,20],[10,0],[0,0]]}),
-      new Land({ RLRegistry: "Delkanda", Extent: 25, ParentLandID: "nil", Owner: "Brad", Boundaries: [[0,20],[10,20],[10,0],[0,0]]})
+      new Land({ id: "1", RLRegistry: "Colombo", Extent: 50, ParentLandID: "nil", Owner: "Tomoko", Boundaries: [[0,20],[10,20],[10,0],[0,0]]}),
+      new Land({ id: "2", RLRegistry: "Delkanda", Extent: 25, ParentLandID: "nil", Owner: "Brad", Boundaries: [[0,20],[10,20],[10,0],[0,0]]})
     ];
     await Promise.all(mockData.map(land => land.save()));
   }
